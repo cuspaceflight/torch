1d9c75cabe9c987f3b67b89cbdd17708111be444
dug20 <daniel.u.gibbons@gmail.com>
2021-05-23 20:17:21 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""
+Allows installation via pip, e.g. by navigating to this directory with the command prompt, and using 'pip install .'
+"""
+
+import sys
+from setuptools import setup, find_packages
+
+setup(
+    name='torch',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=['numpy', 'matplotlib', 'scipy', 'ambiance', 'thermo', 'gas_dynamics'],
+    description='Python version of NASA aerodynamic heating program NQLDW019',
+)
